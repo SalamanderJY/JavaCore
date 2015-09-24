3518ec7a1269c7ec42ef2368cc1772bbd8bc3351
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2015-09-24 17:01:06 +0800

    Lock
    Lock

---
+++ TransferRunnable.java
@@ -0,0 +1,33 @@
+package com;
+
+public class TransferRunnable implements Runnable
+{
+    //Constructs a transfer runnable
+	public TransferRunnable(Bank b, int from, double max)
+    {
+    	bank = b;
+    	fromAccount = from;
+    	maxAmount = max;
+    }
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try
+		{
+			while(true)
+			{
+				int toAccount = (int) (bank.size() * Math.random());
+				double amount = maxAmount * Math.random();
+				bank.transfer(fromAccount, toAccount, amount);
+				Thread.sleep((int) (DELAY * Math.random()));
+			}
+		}
+		catch(InterruptedException e)
+		{}
+	}
+    
+	private Bank bank;
+	private int fromAccount;
+	private double maxAmount;
+	private int DELAY = 10;
+}
