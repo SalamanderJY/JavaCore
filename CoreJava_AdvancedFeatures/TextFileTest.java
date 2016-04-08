91bb9a76b526203928e2e452e9ad46723fae7f6d
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2016-04-08 09:06:16 +0800

    高级特性

---
+++ CoreJava_AdvancedFeatures/TextFileTest.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class TextFileTest {
+
+}
+
+class Employee {
+	private String name;
+	private double salary;
+	private Date hireDay;
+	
+	public Employee() {}
+	
+	public Employee(String n, double s, int year, int month, int day) {
+		name = n;
+		salary = s;
+		GregorianCalendar calendar = new GregorianCalendar(year, month-1, day);
+		hireDay = calendar.getTime();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public Date getHireDay() {
+		return hireDay;
+	}
+
+	public void setHireDay(Date hireDay) {
+		this.hireDay = hireDay;
+	}
+}
